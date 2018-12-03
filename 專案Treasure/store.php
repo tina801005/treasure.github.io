8aa1fbe69f713325371bf88b1ada0610b65cca69
tina801005 <45484508+tina801005@users.noreply.github.com>
2018-12-03 16:40:51 +0800

    Add files via upload

---
+++ 專案Treasure/store.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Store</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+
+	<body>
+	<header class="header">
+			<!--logo-->
+	  <div class="heading">
+				<h1>
+					<a href="#">
+						<img class="logo" src="image/logo/logo-treasure.png" alt="treasure">
+					</a>
+				</h1>
+		</div>
+		<!--nav-->
+		<nav class="nav">
+			<ul>
+					<li class="nav-item"><a href="#about">ABOUT</a></li>
+					<li class="nav-item"><a href="#news">NEWS</a></li>
+					<li class="nav-item"><a href="#menu">MENU</a></li>
+					<li class="nav-item"><a href="#contact">CONTACT</a></li>
+					<li class="nav-item"><a href="store.php">SHOP</a></li>
+					<li class="nav-item"><a href="member.html">MEMBER</a></li>
+		  </ul>
+	  </nav>
+			
+			<!--info-->
+			<div class="info">
+				<p class="info-text">營業時間：一~日 10:00a.m.~9:00a.m.</p>
+				<p class="info-text">地址：新北市永和區</p>
+				<p class="info-text">連絡電話：0910-324-182</p>
+			</div>
+			
+			<!--footer-->
+			<footer class="footer">
+				<p class="copyright"><a href="#copyright"  class="copyrightlink">Copyright</a> &copy; 2018 Treasure <a href="#" class="designer">Designer</a></p>
+			</footer>
+	</header>
+		<main class="main"></main>
+	</body>
+</html>
